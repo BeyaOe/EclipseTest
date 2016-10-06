755487fedb4a0f4b9b6357c126581e745aed2601
Denny Flaemig <Denny.Flaemig@ids.de>
2016-10-06 13:48:16 +0200

    nochmal

--- src/main/java/de/ids/demo/TestProgramm.java
+++ src/main/java/de/ids/demo/TestProgramm.java
@@ -1,14 +1,20 @@
 package de.ids.demo;
 
 public class TestProgramm {
-	
+
 	double a;
 	double o;
-	
+
 	public double addition(double a, double o) {
-		
-		double c = a+o;
+
+		double c = a + o;
 		return c;
+	}
+
+	public double multiply(double a, double b) {
+
+		double d = a * b;
+		return d;
 	}
 
 }
